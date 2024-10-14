aab99424be2e1c00182c969f73724273b6daa9d5
Santiago Ariel Mansilla <56666653+Gann4Life@users.noreply.github.com>
2024-10-14 20:19:47 -0300

    Ignore maya and prism-launcher

---
+++ scripts/Configure.ps1
@@ -0,0 +1,7 @@
+# The purpose of the script is to make questions and choose
+# what to do based on the answer.
+# Examples
+# Configuring yasb, komorebi, powershell profile, activate windows, and other stuff.
+
+
+
