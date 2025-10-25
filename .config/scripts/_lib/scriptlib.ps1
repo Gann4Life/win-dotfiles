3cb412f5bbee49509de3de12bc1e920d34ead491
Santiago <56666653+Gann4Life@users.noreply.github.com>
2025-10-25 02:46:34 -0300

    fix(floating-windows): improved behaviour
    Floating window behaviours has been improved, by closing the previous floating window.
    However this assumes that the user won't ever use multiple floating windows and they will always be tiled.

--- .config/scripts/_lib/scriptlib.ps1
+++ .config/scripts/_lib/scriptlib.ps1
@@ -3,15 +3,34 @@ function Start-FloatingWindow {
         [scriptblock]$ScriptBlock = {}
     )
     
+    # Force close existing floating windows
+    try {
+        $state = komorebic state | ConvertFrom-Json
+        
+        # Get all floating wezterm windows 
+        $floatingWeztermWindows = $state.monitors.elements.workspaces.elements.floating_windows.elements | 
+            Where-Object { $_.class -eq "floating" -and $_.exe -eq "wezterm-gui.exe" }
+        
+        # Kill them all
+        foreach ($window in $floatingWeztermWindows) {
+            $proc = Get-Process | Where-Object { $_.MainWindowHandle -eq $window.hwnd }
+            if ($proc) {
+                Stop-Process -Id $proc.Id -Force -ErrorAction SilentlyContinue
+            }
+        }
+    } catch {
+        # Silently continue
+    }
+    
     if ($ScriptBlock.ToString().Trim() -eq '') {
-        # Just launch empty wezterm
-        wezterm-gui start --class floating -- powershell -NoProfile
+        Start-Process wezterm-gui -ArgumentList "start", "--class", "floating", "--", "powershell", "-NoProfile"
     } else {
         $tempScript = [System.IO.Path]::GetTempFileName() + ".ps1"
         $ScriptBlock.ToString() | Out-File -FilePath $tempScript -Encoding UTF8
-        wezterm-gui start --class floating -- powershell -NoProfile -File $tempScript
+        Start-Process wezterm-gui -ArgumentList "start", "--class", "floating", "--", "powershell", "-NoProfile", "-File", $tempScript
     }
 }
+
 
 function Send-Notification {
 	param(
@@ -21,3 +40,19 @@ function Send-Notification {
 
 	python $HOME\.config\scripts\_lib\notify.py $Title $Description
 }
+
+# ZOMBIE CODE
+# function Start-FloatingWindow {
+#     param(
+#         [scriptblock]$ScriptBlock = {}
+#     )
+#     
+#     if ($ScriptBlock.ToString().Trim() -eq '') {
+#         # Just launch empty wezterm
+#         wezterm-gui start --class floating -- powershell -NoProfile
+#     } else {
+#         $tempScript = [System.IO.Path]::GetTempFileName() + ".ps1"
+#         $ScriptBlock.ToString() | Out-File -FilePath $tempScript -Encoding UTF8
+#         wezterm-gui start --class floating -- powershell -NoProfile -File $tempScript
+#     }
+# }
